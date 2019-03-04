9184262d9630eddce6bb03b8846636bc5fb98a03
colinskeep <colinjinskeep@gmail.com>
2019-03-03 18:24:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# MongoDB Event Listener  
+
+Simple application to log changes to a remote database to your console.  
+
+## Install  
+```$npm install```  
+
+```$cp .env.example .env```  
+
+```$vi .env```  
+
+insert credentials  
+
+## Running
+
+```$node index.js```
---
+++ index.js
@@ -0,0 +1,20 @@
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, {useNewUrlParser: true}`;
+const pipeline = [
+  {
+    $project: { documentKey: false }
+  }
+];
+
+MongoClient.connect(url, function(err, client) {
+  if (!err) {
+    const db = client.db('users');
+    const collection = db.collection(process.env.DB_COLLECTION);
+    let changeStream = collection.watch(pipeline);
+    changeStream.on('change', function(change){
+      //do stuff here
+      console.log(change);
+    })
+  }
+});
